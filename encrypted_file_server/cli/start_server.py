77ec07167aad4cc79a27f3a1fb9d70964a90d129
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-01-25 20:49:13 +0800

    Clean up code

--- encrypted_file_server/cli/start_server.py
+++ encrypted_file_server/cli/start_server.py
@@ -6,7 +6,8 @@ import subprocess
 def run():
     try:
         subprocess.run(
-            ["gunicorn", "-w", "4", "-b", "0.0.0.0", "--log-level", "debug", "encrypted_file_server.server:create_app()"]
+            ["gunicorn", "-w", "4", "-b", "0.0.0.0", "--log-level", "debug",
+             "encrypted_file_server.server:create_app()"]
         )
     except KeyboardInterrupt:
         print("Server stopped.")
